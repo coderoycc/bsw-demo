e79f9ebaeb6c177dbf328d6219b460d5b02a58c8
coderoycc <rcchambi4@gmail.com>
2026-03-31 22:51:59 -0400

    refactor: package bottom sheet install

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { DynamicBottomSheet } from "@coderoycc/bottom-sheet-wrappers";
+const open = ref(false)
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+    <button @click="open = true">Open</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <dynamic-bottom-sheet v-model="open">
+      <template #header>
+        <div class="header-custom">
+          <span>Hola mundo</span>
+          <button @click="open = false">X</button>
+        </div>
+      </template>
+      <div class="border">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+      </div>
+    </dynamic-bottom-sheet>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.border {
+  border: 1px solid red;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.header-custom {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+import "@coderoycc/bottom-sheet-wrappers/dist/bottom-sheet-wrappers.css";
+import BottomSheetPlugin from "@coderoycc/bottom-sheet-wrappers";
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(BottomSheetPlugin)
+app.mount('#app')
